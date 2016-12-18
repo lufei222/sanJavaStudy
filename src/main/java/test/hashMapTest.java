03d0698fa2f6e3f5c60bed121ae5b4eba830145c
1132647424@qq.com <123asdfgh>
2016-12-19 00:00:48 +0800

    研究hashmap

---
+++ src/main/java/test/hashMapTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by luosan on 2016/12/18.
+ */
+public class hashMapTest {
+    Map<Integer,Integer> a=new HashMap<Integer, Integer>();
+}
