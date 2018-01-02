b1c08d2c29345ff0b153129e5d0bd4d872477538
luosan <1132647424@qq.com>
2018-01-02 21:01:59 +0800

    hashmap

---
+++ src/main/java/stu_2018/ci_map.java
@@ -0,0 +1,20 @@
+package stu_2018;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ci_map {
+
+    public static void main(String[] args) {
+        Map<String,String> cusHashMap =new HashMap<String, String>();
+
+        cusHashMap.put("1","1");
+        cusHashMap.put("1","2");
+        cusHashMap.get("1");
+
+    }
+
+
+
+
+}
