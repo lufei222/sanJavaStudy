b15cb883779b0cd13d5f79d9935b83b8e5299297
luosan <1132647424@qq.com>
2018-01-13 11:44:06 +0800

    多线程之实现callable接口，重写call方法 ，等待线程结束，并返回结果

--- src/main/java/stu_2018/testStringBuffer.java
+++ src/main/java/stu_2018/testStringBuffer.java
@@ -1,6 +1,8 @@
 package stu_2018;
 
 import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class testStringBuffer {
     public static void main(String[] args) {
@@ -17,16 +19,14 @@ public class testStringBuffer {
         for( int i=0;i <10;i++ ) //给数组增加10个Int元素
             list.add(i);
 //..程序做一些处理
-        list.re
-
         list.remove(5);//将第6个元素移除
         for( int i=0;i <3;i++ ) //再增加3个元素
             list.add(i+20);
         Integer [] values = (Integer[])list.toArray();//返回ArrayList包含的数组
-
-
+        Float a= 1.0f%0;
 
         ArrayList<String> s = new ArrayList<String>();
+
 
 s.add("a");
 s.add("b");
